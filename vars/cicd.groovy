53e51f4f31311108219daf74041b15833c051f41
sreeshanmukha <121486234+sreeshanmukha@users.noreply.github.com>
2023-01-23 17:30:14 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,19 @@
+def add(a,b)
+{
+	echo "The sum of ${a} and ${b} is ${a+b}"
+}
+
+def sub(a,b)
+{
+	echo "The subraction of ${b} from ${a} is ${a-b}"
+}
+
+def mul(a,b)
+{
+	echo "The multiplication of ${a} and ${b} is ${a*b}"
+}
+
+def dev(a,b)
+{
+  echo "The devision of ${a} and ${b} is ${a/b}"
+ }
